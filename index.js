d9df74248d51c12792754426f66f5c3c954cb4d1
JozianeNascimento <zianenascimento@hotmail.com>
2022-10-13 00:23:13 -0300

    Implementação da API telegraf e teste de integração das duas APIs internas

--- index.js
+++ index.js
@@ -8,18 +8,27 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+require('dotenv-safe').config();
+const { Telegraf } = require('telegraf');
 const api_geo = require("./api_nominatim");
 const express = require("express");
 const server = express();
 server.use(express.json());
-server.listen(8080);
+server.listen(3000);
 server.get("/", (req, res) => {
     return res.send({ message: "no ar" });
 });
 server.get("/localizacao/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
     try {
-        const { data } = yield api_geo.get(`reverse?format=json&${id}`); //format=json&lat=-22.3577&lon=-47.3849
+        const { data } = yield api_geo.get(`reverse?format=json&${id}`); //lat=-22.3577&lon=-47.3849
+        //const {data} = await api_geo.get("reverse?format=json&format=json&lat=-22.3577&lon=-47.3849"); //format=json&lat=-22.3577&lon=-47.3849
+        var dados = data;
+        const bot = new Telegraf(process.env.BOT_TOKEN);
+        if (dados = !null) {
+            dados = JSON.stringify(data, ['display_name']);
+            bot.telegram.sendMessage(process.env.CHAT_ID, dados);
+        }
         return res.send({ data });
     }
     catch (error) {
--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+require('dotenv-safe').config();
+
+const { Telegraf } = require('telegraf')
+ 
 const api_geo = require("./api_nominatim");
 
 const express = require("express");
@@ -6,7 +10,7 @@ const server = express();
 
 server.use(express.json());
 
-server.listen(8080);
+server.listen(3000);
 
 server.get("/",(req, res) => {
     return res.send({message: "no ar"});
@@ -15,11 +19,18 @@ server.get("/",(req, res) => {
 server.get("/localizacao/:id",async (req, res) => {
     const { id } = req.params;
     try{
-        const {data} = await api_geo.get(`reverse?format=json&${id}`); //format=json&lat=-22.3577&lon=-47.3849
-
+        const {data} = await api_geo.get(`reverse?format=json&${id}`); //lat=-22.3577&lon=-47.3849
+        //const {data} = await api_geo.get("reverse?format=json&format=json&lat=-22.3577&lon=-47.3849"); //format=json&lat=-22.3577&lon=-47.3849
+        var dados =data;
+        const bot = new Telegraf(process.env.BOT_TOKEN);
+        if (dados =! null){
+            dados = JSON.stringify(data,['display_name'])
+            bot.telegram.sendMessage(process.env.CHAT_ID, dados );
+        }
         return res.send({data});
     } catch(error){
         res.send({error: error.message})
     }
     
 })
+
